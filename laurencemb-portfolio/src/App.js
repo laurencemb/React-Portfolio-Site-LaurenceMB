3d773449af6625ac26d81ccf4507d23dc024b9c8
Laurence Burt <laurencemb@hotmail.co.uk>
2023-03-13 15:00:26 +0000

    added start files for source and pages and navbar component, got navbar to appear on screen unstyled

--- laurencemb-portfolio/src/App.js
+++ laurencemb-portfolio/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import ProjectIndv from './pages/ProjectIndv';
+import Contact from './pages/Contact'; 
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Wrapper from './components/Wrapper'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+return (
+    <Router>
+        <div>
+            <Navbar />
+             <Wrapper>
+                 <Routes>
+                    <Route path ='/' element = {<Home/>} />
+                    <Route path ='/projects' element = {<Projects/>} />
+                    <Route path ='/project' element = {<ProjectIndv/>} />
+                    <Route path ='/contact' element = {<Contact/>} />
+                </Routes> 
+            </Wrapper>
+            <Footer/>
+        </div>
+    </Router>
+)
 }
 
 export default App;
---
+++ laurencemb-portfolio/src/components/Navbar/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
+import './style.css'
+
+function Navbar () {
+    return (
+        <nav className='navbar'>
+            <Link className="navbar-brand" to="/">
+        Laurence
+      </Link>
+            <div>
+                <ul className='navbar-nav'>
+                    <li className='nav-item'>
+                        <NavLink  
+                            to='/'
+                            end
+                            className={({isActive}) =>
+                                isActive ? 'nav-link active' : 'nav-link'
+                            }
+                            >
+                            About 
+                        </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink  
+                            to='/projects'
+                            className={({isActive}) =>
+                                isActive ? 'nav-link active' : 'nav-link'
+                            }
+                            >
+                           My Projects 
+                        </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink  
+                            to='/contact'
+                            className={({isActive}) =>
+                                isActive ? 'nav-link active' : 'nav-link'
+                            }
+                            >
+                            Contact Me
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
